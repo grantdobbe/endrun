9623751181142e9956206ecb9e88ccf561ca6b24
Grant Dobbe <grant@dobbe.us>
2014-04-23 13:16:38 -0500

    - added a post-commit call to the config.rb file

--- install/config.rb
+++ install/config.rb
@@ -13,3 +13,6 @@
 Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
 Gollum::Page::FORMAT_NAMES = { :markdown  => "Markdown" }
 
+Gollum::Hook.register(:post_commit, :hook_id) do |committer, sha1|
+  system 'dtn-transmit.py nodeX && cat /tmp/nodeX.bundle.dtn > /tmp/tobesent'
+end
