5791395a9201ebf4ed7aa3ca63471828d04c4e74
Grant Dobbe <grant@dobbe.us>
2014-04-22 20:54:55 -0500

    - changed sed delineators

--- install/node-config.sh
+++ install/node-config.sh
@@ -18,13 +18,13 @@ mv $HOME/$NODE-deploy $HOME/plp
 
 echo "changing host-specific settings"
 sudo sed -i "s/$HOSTNAME/$NODE/g" /etc/hostname
-sudo sed -i "s/127\.0\.1\.1\s$HOSTNAME/127\.0\.1\.1\s$NODE/g" /etc/hosts
+sudo sed -i "s/127\.0\.1\.1	$HOSTNAME/127\.0\.1\.1	$NODE/g" /etc/hosts
 sudo cp /home/$USER/libnatasha/install/gollum-server /etc/init.d/
 sudo chmod 755 /etc/init.d/gollum-server
 cp $HOME/libnatasha/install/config.rb $HOME/plp/config.rb
 sudo update-rc.d gollum-server defaults
 
 echo "changing bundle paths"
-sed -i "s/\/home\/gdobbe\/plp-test\/$NODE-deploy/$HOME\/plp/g" /home/$USER/plp/repo/.git/config
+sed -i "s;/home/gdobbe/plp-test/$NODE-deploy;$HOME/plp;g" $HOME/plp/repo/.git/config
 
 echo "all done!"
